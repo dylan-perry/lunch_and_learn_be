470216cf510109f687a9d318eb71a52e54a3750d
Dylan Perry <dylanbperry@gmail.com>
2024-03-05 13:17:23 -0800

    Add sad path testing for US-5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,22 @@ class ApplicationController < ActionController::API
     def not_found_response
         render json: { error: 'Sorry, your credentials are bad!' }, status: :unauthorized
     end
+
+    # AR helpers
+
+    def find_user_by_email(email)
+        if user = User.find_by(email: email)
+            user
+        else
+            raise ActiveRecord::RecordNotFound
+        end
+    end
+
+    def find_user_by_api_key(api_key)
+        if user = User.find_by(api_key: api_key)
+            user
+        else
+            raise ActiveRecord::RecordNotFound
+        end
+    end
 end
